e7397b9cff6a62704a83ab55ed186634a794c382
Dr Mark C. Sinclair <markcsinclair@gmail.com>
2024-11-08 14:09:46 +0000

    add six more test cases for split_str_2

--- split_str_2_test.cpp
+++ split_str_2_test.cpp
@@ -2,9 +2,52 @@
 
 #include "split_str_2_lib.h"
 
+
+TEST(SplitStr2Test, GetTokensSingleWord) {
+  string str("hello");
+  vector<string> tokens   = getTokens(str);
+  vector<string> expected = { "hello" };
+  EXPECT_TRUE(expected == tokens);
+}
+
+TEST(SplitStr2Test, GetTokensSingleWordSpaceBefore) {
+  string str(" hello");
+  vector<string> tokens   = getTokens(str);
+  vector<string> expected = { "hello" };
+  EXPECT_TRUE(expected == tokens);
+}
+
+TEST(SplitStr2Test, GetTokensSingleWordSpaceAfter) {
+  string str("hello ");
+  vector<string> tokens   = getTokens(str);
+  vector<string> expected = { "hello" };
+  EXPECT_TRUE(expected == tokens);
+}
+
+TEST(SplitStr2Test, GetTokensTwoWords) {
+  string str("hello world");
+  vector<string> tokens   = getTokens(str);
+  vector<string> expected = { "hello", "world" };
+  EXPECT_TRUE(expected == tokens);
+}
+
+TEST(SplitStr2Test, GetTokensTwoWordsDoubleSpaceBetween) {
+  string str("hello  world");
+  vector<string> tokens   = getTokens(str);
+  vector<string> expected = { "hello", "world" };
+  EXPECT_TRUE(expected == tokens);
+}
+
 TEST(SplitStr2Test, GetOutputSingleToken) {
   vector<string> tokens = { "hello" };
   string output = getOutput(tokens);
   string expected("hello\n");
+  EXPECT_TRUE(expected == output);
+}
+
+TEST(SplitStr2Test, GetOutputTwoTokens) {
+  vector<string> tokens = { "hello", "world" };
+  string output = getOutput(tokens);
+  string expected("hello\nworld\n");
   EXPECT_TRUE(expected == output);
 }
